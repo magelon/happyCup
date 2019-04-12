5cca4b047883cd489bd7eab3cbe1ed281d7d4d78
magelon <alfardproject@gmail.com>
2019-04-12 10:37:27 -0700

    c

---
+++ Assets/happyglass/Scripts/LineCreator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineCreator : MonoBehaviour
+{
+    public GameObject linePrefab;
+    private GameObject lineGo;
+    DrawLineWithCollider activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lineGo = Instantiate(linePrefab);
+            activeLine = lineGo.GetComponent<DrawLineWithCollider>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+            lineGo.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
+    }
+}
---
+++ Assets/happyglass/Scripts/cupFillState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cupFillState : MonoBehaviour
+{
+    public float time;
+    void OnTriggerStay2D(Collider2D other)
+    {
+        time += Time.time;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        time = 0;
+    }
+}
